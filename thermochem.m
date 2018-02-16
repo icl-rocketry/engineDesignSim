b82881885e3f09bbcc105e7269e14304cbdd2e73
Dev <dra16@ic.ac.uk>
2018-02-16 17:10:45 +0000

    updates to interp

--- thermochem.m
+++ thermochem.m
@@ -1,12 +1,18 @@
-function [T_flame, gamma, m_mol, R,c_star] = thermochem(OF,etac)
+function [T_flame, gamma, m_mol, R,c_star] = thermochem(OF,P_cc,etac)
 
+%interp uses X is P_cc and Y is OF
 
-%changes
+%load P_cc_vals OF_vals T_flame_data gamma_data m_mol_data
 
-
-T_flame = 3300;     %[K] should be determined properly
-gamma = 1.24;       %    should be determined properly
+T_flame = 3300;
+gamma = 1.24;
 m_mol = 0.0262109;  %Molar mass (kg/mol) should be determined properly
+
+
+%T_flame = interp2(P_cc_vals,OF_vals,T_flame_data,P_cc,OF); %[K] 
+%gamma = interp2(P_cc_vals,OF_vals,gamma_data,P_cc,OF); %dimensionless
+%m_mpl = interp2(P_cc_vals,OF_vals,m_mol_data,P_cc,OF); %dimensionless
+
 R = 8.314/m_mol;    %Specific Gas Constant [SPAD, eq 7 .72]
 %etac is the combustion efficiency, usually about 0.95 [SPAD]
 c_star = etac*sqrt(gamma*R*T_flame)/(gamma*(2/(gamma+1))^((gamma+1)/(2*gamma-2))); %characteristic velocity [SPAD, eq 7.71]
