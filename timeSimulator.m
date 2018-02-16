53313c05119c067a1a11fb9a25e98c32944cda1f
Shreeyam Kacker <shreeyamkacker@gmail.com>
2018-02-16 15:18:09 +0000

    Merged v2

--- timeSimulator.m
+++ timeSimulator.m
@@ -1,35 +1,36 @@
-clear all; 
-clc;
-close all;
-
 %% Version 1
 % Last edited by Dev on Feb 10 2018
 
-%%% This script aims to perform a time-varying simulation of the rocket 
-%%% burn to verify that performance matches expectations in the config file 
-%%% using Chapter 7 of Space Propulsion Analysis and Design (SPAD) and  
+%%% This script aims to perform a time-varying simulation of the rocket
+%%% burn to verify that performance matches expectations in the config file
+%%% using Chapter 7 of Space Propulsion Analysis and Design (SPAD) and
 %%% Rocket Propulsion Elements (RPE)
 
 %This script follows Table 7.9 in SPAD almost exactly.
 %It assumes a constant mdot_ox and circular port geometry
 
-
 %#ok<*SAGROW>
 %the line above hides the error message on vector lenth increasing with
 %each step.
 
+% ayy lmao this is a C H A N G E
+
+%% Run the config file:
+engineConfig_Initial_2018
+
+clear all;
+clc;
+close all;
 %% import configuration parameters
 
+load inputs.mat
 load constants.mat
 load targets.mat
 load configfile.mat
 
-lambda = 1;     %nozzle thrust efficiency factor
-P_amb = 1*bar;  %[Pa] ambient pressure;
-
 D_port(1) = Dport_init;
 
-mdot_ox = mdot_oxinit; %at the moment I assume we can supply this, although we should really be putting in the value that adam baker gives us. 
+mdot_ox = mdot_oxinit; %at the moment I assume we can supply this much mass flow rate, although we should really be putting in the value that adam baker gives us.
 
 expansionRatio=A_exit/A_throat;
 
@@ -50,7 +51,7 @@ thresh = 1e-6; % [kg m^-2 s^-1]threshold difference in flow flux for iteratively
 while qburnfin == 0
     
     % 2. determine fuel regression rate & % 3. determine total mass flow rate
-        
+    
     G_ox(ti) = 4*mdot_ox/(pi*(D_port(ti))^2);
     
     G_prop(ti) = G_ox(ti); %as a starting point
@@ -80,7 +81,7 @@ while qburnfin == 0
     end
     
     mdot_prop(ti)=mdot_ox+mdot_fuel(ti); %[kg/s]
-
+    
     % 4. determine thermochemistry
     
     OF(ti)=mdot_ox/mdot_fuel(ti);
@@ -113,80 +114,102 @@ while qburnfin == 0
     D_port(ti+1) = D_port(ti)+2*rdot(ti)*deltaT;
     
     ti=ti+1; %increment the time index
-     t=t+deltaT; %increment the time
-     
+    t=t+deltaT; %increment the time
+    
     % 8. check if burn is completed, and go to next step
     
-     %qburnfin = checkburnfin(); % a full function could be written to
-     %perform this check properly.At the moment it just checks for burn
-     %time completion.
-     
-     %Things to check:
-     % (1) chamber pressure < ox tank pressure
-     % (2) Choked flow
-     % (3) fuel is not entirely eaten up
-     
-     if t>t_burn
-         qburnfin=1;
-     else
-         qburnfin=0;
-     end
-     
+    %qburnfin = checkburnfin(); % a full function could be written to
+    %perform this check properly.At the moment it just checks for burn
+    %time completion.
+    
+    %Things to check:
+    % (1) chamber pressure < ox tank pressure
+    % (2) Choked flow
+    % (3) fuel is not entirely eaten up
+    
+    if fuelweb(end)<=0 %using only condition 3 for now
+        qburnfin=1;
+    else
+        qburnfin=0;
+    end
+    
 end
 
-%%
-I_total_result = sum(F)*deltaT;
+fuelweb = fuelweb(1:end-1);
+D_port = D_port(1:end-1);
 
+%%
+I_total_result = sum(F)*deltaT
+
+t_burn_result = t
+
+t_axis = 0:deltaT:t_burn_result;
+
+F_init_result  = F(1)
+
+Isp_avg = mean(Isp)
+
+save sim_results.mat I_total_result F_init_result
 
 %% plot results
 
-figure;
-plot(F)
-title('F')
+qplot = 1;
 
-figure;
-plot(c_star)
-title('c star')
+if qplot == 1
+    
+    figure(1);
+    
+    subplot(3,4,1)
+    plot(t_axis,F,[0],[0])
+    title('F')
 
-figure;
-plot(D_port)
-title('D port')
+    subplot(3,4,2)
+    plot(t_axis,c_star,[0],[0])
+    title('c star')
 
-figure;
-plot(G_ox)
-title('G ox')
-
-figure;
-plot(G_fuel)
-title('G fuel')
-
-figure;
-plot(G_prop)
-title('G prop')
-
-figure;
-plot(mdot_fuel)
-title('mdot fuel')
-
-figure;
-plot(Isp)
-title('Isp')
-
-figure;
-plot(M_exit)
-title('M exit')
-
-figure;
-plot(OF)
-title('OF')
-
-figure;
-plot(P_cc)
-title('P cc')
-
-figure;
-plot(P_exit)
-title('P exit')
+    
+    subplot(3,4,3)
+    plot(t_axis,D_port,[0],[0])
+    
+    title('D port')
+    
+    subplot(3,4,4)
+    plot(t_axis,G_ox,[0],[0])
+    title('G ox')
+    
+    subplot(3,4,5)
+    plot(t_axis,G_fuel,[0],[0])
+    title('G fuel')
+    
+    subplot(3,4,6)
+    plot(t_axis,G_prop,[0],[0])
+    title('G prop')
+    
+    subplot(3,4,7)
+    plot(t_axis,mdot_fuel,[0],[0])
+    title('mdot fuel')
+    
+    subplot(3,4,8)
+    plot(t_axis,Isp,[0],[0])
+    title('Isp')
+    
+    subplot(3,4,9)
+    plot(t_axis,M_exit,[0],[0])
+    title('M exit')
+    
+    subplot(3,4,10)
+    plot(t_axis,OF,[0],[0])
+    title('OF')
+    
+    subplot(3,4,11)
+    plot(t_axis,P_cc,[0],[0])
+    title('P cc')
+    
+    subplot(3,4,12)
+    plot(t_axis,P_exit,[0],[0])
+    title('P exit')
+    
+end
 
 
 
